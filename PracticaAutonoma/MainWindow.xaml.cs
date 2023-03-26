f2c3cbffe3dbe90a3c7275196170ca726a3b1b38
Daniel Bazo <77023868+danibcorr@users.noreply.github.com>
2023-03-26 16:46:07 +0200

    Incorporación avanzar y retroceder diapositiva.
    Se debe cambiar la simulación de las teclas, en esta versión va hacia arriba o abajo. Existe un Bug donde al ir hacia arriba sigue funcionando a pesar de las variables de control. Se han removido las líneas de las piernas. Se ha modularizado el código para simplificar su entendimiento y facilitar la incorporación de nuevas características.

--- PracticaAutonoma/MainWindow.xaml.cs
+++ PracticaAutonoma/MainWindow.xaml.cs
@@ -1,60 +1,31 @@
 ﻿namespace Microsoft.Samples.Kinect.ColorBasics
 {
     // Librerias a utilizar
-    using System;
-    using System.Globalization;
     using System.IO;
     using System.Windows;
     using System.Windows.Media;
     using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
-    using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
-    using System.Windows.Input;
     using Microsoft.Kinect;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using WindowsInput;
+    using static Esqueleto;
+    using static RGB;
 
     public partial class MainWindow : Window
     {
-        // Variables para la camara RGB
-        /// Active Kinect sensor
+        // Variables código principal
+        // Variable utilizada para la simulación de pulsaciones de teclas
+        InputSimulator sim = new InputSimulator();
+        // Variable para la activación del sensor Kinect
         private KinectSensor sensor;
-        /// Bitmap that will hold color information
-        private WriteableBitmap colorBitmap;
-        /// Intermediate storage for the color data received from the camera
-        private byte[] colorPixels;
-
-
-        // Variables para el esqueleto
-        /// Width of output drawing
-        private const float RenderWidth = 640.0f;
-        /// Height of our output drawing
-        private const float RenderHeight = 480.0f;
-        /// Thickness of drawn joint lines
-        private const double JointThickness = 3;
-        /// Thickness of body center ellipse
-        private const double BodyCenterThickness = 10;
-        /// Thickness of clip edge rectangles
-        private const double ClipBoundsThickness = 10;
-        /// Brush used to draw skeleton center point
-        private readonly Brush centerPointBrush = Brushes.Blue;
-        /// Brush used for drawing joints that are currently tracked
-        private readonly Brush trackedJointBrush = new SolidColorBrush(Color.FromArgb(255, 68, 192, 68));
-        /// Brush used for drawing joints that are currently inferred
-        private readonly Brush inferredJointBrush = Brushes.Yellow;
-        /// Pen used for drawing bones that are currently tracked
-        private readonly Pen trackedBonePen = new Pen(Brushes.Green, 6);
-        /// Pen used for drawing bones that are currently inferred
-        private readonly Pen inferredBonePen = new Pen(Brushes.Gray, 1);
-        /// Drawing group for skeleton rendering output
-        private DrawingGroup drawingGroup;
-        /// Drawing image that we will display
-        private DrawingImage imageSource;
+        // Definición de variables para avanzar o retroceder diapositivas
+        // Definición de los colores para las elipses de las manos
+        private readonly Brush brushManoAlzadaIzqda = Brushes.Aquamarine;
+        private readonly Brush brushManoAlzadaDerecha = Brushes.Yellow;
+        // Definición de los radios de las elipses, el valor es el radio
+        private const double JointThicknessAzul = 10, JointThicknessAmarillo = 20;
+        private float miAlturaIzqda = 0, miAlturaDerecha = 0, miAlturaCabeza = 0;
+        // Variables para el control de las diapositivas
+        bool control_der = false, control_izq = false;
 
 
         /// Initializes a new instance of the MainWindow class.
@@ -63,62 +34,19 @@
             InitializeComponent();
         }
 
-        // Función esqueleto
-        /// Draws indicators to show which edges are clipping skeleton data
-        private static void RenderClippedEdges(Skeleton skeleton, DrawingContext drawingContext)
-        {
-            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Bottom))
-            {
-                drawingContext.DrawRectangle(
-                    Brushes.Red,
-                    null,
-                    new Rect(0, RenderHeight - ClipBoundsThickness, RenderWidth, ClipBoundsThickness));
-            }
-
-            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Top))
-            {
-                drawingContext.DrawRectangle(
-                    Brushes.Red,
-                    null,
-                    new Rect(0, 0, RenderWidth, ClipBoundsThickness));
-            }
-
-            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Left))
-            {
-                drawingContext.DrawRectangle(
-                    Brushes.Red,
-                    null,
-                    new Rect(0, 0, ClipBoundsThickness, RenderHeight));
-            }
-
-            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Right))
-            {
-                drawingContext.DrawRectangle(
-                    Brushes.Red,
-                    null,
-                    new Rect(RenderWidth - ClipBoundsThickness, 0, ClipBoundsThickness, RenderHeight));
-            }
-        }
-
-
+  
         /// Execute startup tasks
-
-        /// <param name="sender">object sending the event</param>
-        /// <param name="e">event arguments</param>
         private void WindowLoaded(object sender, RoutedEventArgs e)
         {
             // Colocar Esqueleto en la ventana
             // Create the drawing group we'll use for drawing
-            this.drawingGroup = new DrawingGroup();
+            drawingGroup = new DrawingGroup();
             // Create an image source that we can use in our image control
-            this.imageSource = new DrawingImage(this.drawingGroup);
+            imageSource = new DrawingImage(drawingGroup);
             // Display the drawing using our image control
-            kinectEsqueleto.Source = this.imageSource;
+            kinectEsqueleto.Source = imageSource;
 
             // Look through all sensors and start the first connected one.
-            // This requires that a Kinect is connected at the time of app startup.
-            // To make your app robust against plug/unplug, 
-            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit (See components in Toolkit Browser).
             foreach (var potentialSensor in KinectSensor.KinectSensors)
             {
                 if (potentialSensor.Status == KinectStatus.Connected)
@@ -134,9 +62,9 @@
                 // Turn on the color stream to receive color frames
                 this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
                 // Allocate space to put the pixels we'll receive
-                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+                colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
                 // This is the bitmap we'll display on-screen
-                this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+                colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
                 // Add an event handler to be called whenever there is new color frame data
                 this.sensor.ColorFrameReady += this.SensorColorFrameReady;
 
@@ -202,7 +130,7 @@
                 }
             }
 
-            using (DrawingContext dc = this.drawingGroup.Open())
+            using (DrawingContext dc = drawingGroup.Open())
             {
                 // Draw a transparent background to set the render size
                 dc.DrawRectangle(Brushes.Black, null, new Rect(0.0, 0.0, RenderWidth, RenderHeight));
@@ -215,112 +143,27 @@
 
                         if (skel.TrackingState == SkeletonTrackingState.Tracked)
                         {
-                            this.DrawBonesAndJoints(skel, dc);
+                            DrawBonesAndJoints(skel, dc, sensor, ref miAlturaIzqda,
+                                 ref miAlturaDerecha, ref miAlturaCabeza, brushManoAlzadaIzqda, brushManoAlzadaDerecha, ref control_der,
+                                 ref control_izq, JointThicknessAzul, JointThicknessAmarillo, sim);
                         }
                         else if (skel.TrackingState == SkeletonTrackingState.PositionOnly)
                         {
                             dc.DrawEllipse(
-                            this.centerPointBrush,
+                            centerPointBrush,
                             null,
-                            this.SkeletonPointToScreen(skel.Position),
+                            SkeletonPointToScreen(skel.Position, sensor),
                             BodyCenterThickness,
                             BodyCenterThickness);
                         }
                     }
+
+                    dc.Close();
                 }
 
                 // prevent drawing outside of our render area
-                this.drawingGroup.ClipGeometry = new RectangleGeometry(new Rect(0.0, 0.0, RenderWidth, RenderHeight));
+                drawingGroup.ClipGeometry = new RectangleGeometry(new Rect(0.0, 0.0, RenderWidth, RenderHeight));
             }
-        }
-
-        private void DrawBonesAndJoints(Skeleton skeleton, DrawingContext drawingContext)
-        {
-            // Render Torso
-            this.DrawBone(skeleton, drawingContext, JointType.Head, JointType.ShoulderCenter);
-            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.ShoulderLeft);
-            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.ShoulderRight);
-            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.Spine);
-            this.DrawBone(skeleton, drawingContext, JointType.Spine, JointType.HipCenter);
-            this.DrawBone(skeleton, drawingContext, JointType.HipCenter, JointType.HipLeft);
-            this.DrawBone(skeleton, drawingContext, JointType.HipCenter, JointType.HipRight);
-
-            // Left Arm
-            this.DrawBone(skeleton, drawingContext, JointType.ShoulderLeft, JointType.ElbowLeft);
-            this.DrawBone(skeleton, drawingContext, JointType.ElbowLeft, JointType.WristLeft);
-            this.DrawBone(skeleton, drawingContext, JointType.WristLeft, JointType.HandLeft);
-
-            // Right Arm
-            this.DrawBone(skeleton, drawingContext, JointType.ShoulderRight, JointType.ElbowRight);
-            this.DrawBone(skeleton, drawingContext, JointType.ElbowRight, JointType.WristRight);
-            this.DrawBone(skeleton, drawingContext, JointType.WristRight, JointType.HandRight);
-
-            // Left Leg
-            this.DrawBone(skeleton, drawingContext, JointType.HipLeft, JointType.KneeLeft);
-            this.DrawBone(skeleton, drawingContext, JointType.KneeLeft, JointType.AnkleLeft);
-            this.DrawBone(skeleton, drawingContext, JointType.AnkleLeft, JointType.FootLeft);
-
-            // Right Leg
-            this.DrawBone(skeleton, drawingContext, JointType.HipRight, JointType.KneeRight);
-            this.DrawBone(skeleton, drawingContext, JointType.KneeRight, JointType.AnkleRight);
-            this.DrawBone(skeleton, drawingContext, JointType.AnkleRight, JointType.FootRight);
-
-            // Render Joints
-            foreach (Joint joint in skeleton.Joints)
-            {
-                Brush drawBrush = null;
-
-                if (joint.TrackingState == JointTrackingState.Tracked)
-                {
-                    drawBrush = this.trackedJointBrush;
-                }
-                else if (joint.TrackingState == JointTrackingState.Inferred)
-                {
-                    drawBrush = this.inferredJointBrush;
-                }
-
-                if (drawBrush != null)
-                {
-                    drawingContext.DrawEllipse(drawBrush, null, this.SkeletonPointToScreen(joint.Position), JointThickness, JointThickness);
-                }
-            }
-        }
-
-        private Point SkeletonPointToScreen(SkeletonPoint skelpoint)
-        {
-            // Convert point to depth space.  
-            // We are not using depth directly, but we do want the points in our 640x480 output resolution.
-            DepthImagePoint depthPoint = this.sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(skelpoint, DepthImageFormat.Resolution640x480Fps30);
-            return new Point(depthPoint.X, depthPoint.Y);
-        }
-
-        private void DrawBone(Skeleton skeleton, DrawingContext drawingContext, JointType jointType0, JointType jointType1)
-        {
-            Joint joint0 = skeleton.Joints[jointType0];
-            Joint joint1 = skeleton.Joints[jointType1];
-
-            // If we can't find either of these joints, exit
-            if (joint0.TrackingState == JointTrackingState.NotTracked ||
-                joint1.TrackingState == JointTrackingState.NotTracked)
-            {
-                return;
-            }
-
-            // Don't draw if both points are inferred
-            if (joint0.TrackingState == JointTrackingState.Inferred &&
-                joint1.TrackingState == JointTrackingState.Inferred)
-            {
-                return;
-            }
-
-            // We assume all drawn bones are inferred unless BOTH joints are tracked
-            Pen drawPen = this.inferredBonePen;
-            if (joint0.TrackingState == JointTrackingState.Tracked && joint1.TrackingState == JointTrackingState.Tracked)
-            {
-                drawPen = this.trackedBonePen;
-            }
-
-            drawingContext.DrawLine(drawPen, this.SkeletonPointToScreen(joint0.Position), this.SkeletonPointToScreen(joint1.Position));
         }
     }
 }
