e15f652cd9985a3fe79cfa757f8b19c0f965c3d2
Clara Rubio-Almagro <101986758+clararubio20@users.noreply.github.com>
2023-05-01 10:28:39 +0200

    añadido comentarios

--- PracticaAutonoma/claseRGB.cs
+++ PracticaAutonoma/claseRGB.cs
@@ -1,10 +1,18 @@
-﻿using System.Windows.Media.Imaging;
+/*
+ *  Código realizado por: Daniel Bazo Correa y Clara Rubio Almagro
+ *  Fecha: 01/05/2023
+ *  Asignatura de Sistemas electrónicos interactivos
+ *  ETSIT Universidad de Málaga
+ *  
+ *  Clase de control de RGB
+ */
+
+// LIBRERIAS
+using System.Windows.Media.Imaging;
 
 public static class RGB
 {
-    // Variables para la camara RGB
-    /// Bitmap that will hold color information
-    public static WriteableBitmap colorBitmap;
-    /// Intermediate storage for the color data received from the camera
-    public static byte[] colorPixels;
+    // VARIABLES PARA CÁMARA RGB
+    public static WriteableBitmap colorBitmap;      // Mapa de bit que contiene la información del color
+    public static byte[] colorPixels;               // Almacenamiento del color recibido por la cámara
 }
